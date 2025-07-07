c10acdccb56fe58e892f317e2e02d265450187a1
Neranjan Dissanayake <114387721+neranjansb@users.noreply.github.com>
2025-07-08 01:12:00 +0530

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-@6(mhc5g=+^f0v&$v@jnkiqc0%v-z73g%6t+&m=&05s&+x=#6m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://localhost:3000/', 'starbound-backend-rxgm.onrender.com']
+ALLOWED_HOSTS = ['http://localhost:3000', 'starbound-backend-rxgm.onrender.com']
 
 
 # Application definition
@@ -76,7 +76,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000/",
+    "http://localhost:3000",
     "https://starbound-backend-rxgm.onrender.com",
 ]
 
