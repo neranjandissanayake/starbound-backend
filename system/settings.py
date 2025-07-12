0bfd961136bad026b62bd14e95c400015c443171
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-12 23:17:05 +0530

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -15,7 +15,6 @@ from datetime import timedelta
 
 import os
 from dotenv import load_dotenv
-import dj_database_url
 
 load_dotenv()
 
@@ -133,8 +132,16 @@ WSGI_APPLICATION = 'system.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
 }
+
 
 
 # Password validation
