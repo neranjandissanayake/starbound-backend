1a01d0065207a161212d2a0f4223e97489a369ff
Neranjan Dissanayake <neranjansb@gmail.com>
2025-07-13 18:23:24 +0530

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-@6(mhc5g=+^f0v&$v@jnkiqc0%v-z73g%6t+&m=&05s&+x=#6m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://localhost:3000', 'https://www.logivis.com', 'https://logivis.com', 'starbound-backend-rxgm.onrender.com']
+ALLOWED_HOSTS = ['http://localhost:3000', 'https://www.logivis.com', 'https://logivis.com']
 
 
 
@@ -85,7 +85,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://www.logivis.com",
     "https://logivis.com",
-    "https://starbound-backend-rxgm.onrender.com",
 ]
 
 REST_FRAMEWORK = {
